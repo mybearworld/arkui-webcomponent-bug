e900fa82322354d19a66530d87d2a5a7d65bcae0
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-21 12:38:19 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Popover } from "@ark-ui/vue";
+</script>
+
+<template>
+  <Popover.Root>
+    <Popover.Trigger> Click Me </Popover.Trigger>
+    <Popover.Positioner>
+      <Popover.Content style="border: 1px solid black">
+        <p>
+          Regular popover content. When this is clicked, the popover will stay
+          open.
+        </p>
+        <p>When clicking the emoji picker below, it'll autoclose.</p>
+        <emoji-picker></emoji-picker>
+      </Popover.Content>
+    </Popover.Positioner>
+  </Popover.Root>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "emoji-picker-element";
+
+createApp(App).mount("#app");
